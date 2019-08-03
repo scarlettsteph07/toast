9615ca0da2bf9741e465b2736e5989ceb4633a2e
Adam Saegebarth <adam.saegebarth@gmail.com>
2019-08-03 12:09:47 -0700

    we dont use tabs!!!!!!

--- toast.js
+++ toast.js
@@ -5,7 +5,7 @@ let { optionalIngredients } = require("./config.json");
 let { numOfOptionalIngredients } = require("./preferences.json");
 
 const randomArrayIndex = array => {
-	return Math.floor(Math.random() * array.length);
+  return Math.floor(Math.random() * array.length);
 };
 
 const toast = [...requiredIngredients];
@@ -14,20 +14,20 @@ toast.push(requiredIngredient2[ingredient2Index]);
 
 if (isCarnivore) {
   const meat = optionalIngredients.find(
-		ingredient => ingredient.name === "meat"
+    ingredient => ingredient.name === "meat"
   );
   toast.push(meat.style[randomArrayIndex(meat.style)]);
   numOfOptionalIngredients--;
 }
 
 for (x = 0; x < numOfOptionalIngredients; x++) {
-	const optionalIngredientIndex = randomArrayIndex(optionalIngredients);
-	const optionalIngredient = optionalIngredients[optionalIngredientIndex];
-	const optionalStyleIndex = randomArrayIndex(optionalIngredient.style);
-	toast.push(optionalIngredient.style[optionalStyleIndex]);
-	optionalIngredients = optionalIngredients.filter((value, i) => {
-		return value !== optionalIngredients[optionalIngredientIndex];
-	});
+  const optionalIngredientIndex = randomArrayIndex(optionalIngredients);
+  const optionalIngredient = optionalIngredients[optionalIngredientIndex];
+  const optionalStyleIndex = randomArrayIndex(optionalIngredient.style);
+  toast.push(optionalIngredient.style[optionalStyleIndex]);
+  optionalIngredients = optionalIngredients.filter((value, i) => {
+    return value !== optionalIngredients[optionalIngredientIndex];
+  });
 }
 
 console.log(toast);
