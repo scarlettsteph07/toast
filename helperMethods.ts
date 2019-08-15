b8500921359c4592542721f20b8f2aae5f3c34c8
Scarlett Tejada <scarlettsteph07@gmail.com>
2019-08-15 16:17:23 -0700

    Rename variables

--- helperMethods.ts
+++ helperMethods.ts
@@ -2,37 +2,36 @@ import { requiredIngredients, requiredIngredient2 } from './config.json'
 
 import { Ingredient, Preferences } from './types/'
 
-export const randomArrayIndex = (array: Array<string> | Array<Ingredient>) => {
-  return Math.floor(Math.random() * array.length);
+export const getRandomArrayIndex = (array: Array<string> | Array<Ingredient>): number => {
+  return Math.floor(Math.random() * array.length)
 }
 
 export const getIngredientsHelper = ({ isCarnivore, numOfOptionalIngredients }: Preferences) => {
-  let { optionalIngredients } = require("./config.json");
-
-  const toast = [...requiredIngredients];
-  const ingredient2Index = randomArrayIndex(requiredIngredient2);
-  toast.push(requiredIngredient2[ingredient2Index]);
+  let { optionalIngredients } = require("./config.json")
+  const ingredientsArray = [...requiredIngredients]
+  const ingredient2Index = getRandomArrayIndex(requiredIngredient2)
+  ingredientsArray.push(requiredIngredient2[ingredient2Index])
 
   if (isCarnivore) {
     const meat = optionalIngredients.find(
       (ingredient: Ingredient) => ingredient.name === "meat"
-    );
-    console.log(meat);
-    meat !== undefined && meat !== null && toast.push(meat.style[randomArrayIndex(meat.style)]);
-    numOfOptionalIngredients--;
+    )
+    console.log(meat)
+    meat !== undefined && meat !== null && ingredientsArray.push(meat.style[getRandomArrayIndex(meat.style)])
+    numOfOptionalIngredients--
   }
 
   for (let x = 0; x < numOfOptionalIngredients; x++) {
-    const optionalIngredientIndex = randomArrayIndex(optionalIngredients);
-    const optionalIngredient = optionalIngredients[optionalIngredientIndex];
-    const optionalStyleIndex = randomArrayIndex(optionalIngredient.style);
-    toast.push(optionalIngredient.style[optionalStyleIndex]);
-    optionalIngredients = optionalIngredients.filter((value: Ingredient) => {
-      return value !== optionalIngredients[optionalIngredientIndex];
-    });
+    const optionalIngredientIndex = getRandomArrayIndex(optionalIngredients)
+    const optionalIngredient = optionalIngredients[optionalIngredientIndex]
+    const optionalStyleIndex = getRandomArrayIndex(optionalIngredient.style)
+    ingredientsArray.push(optionalIngredient.style[optionalStyleIndex])
+    optionalIngredients = optionalIngredients.filter((ingredient: Ingredient) => {
+      return ingredient !== optionalIngredients[optionalIngredientIndex]
+    })
   }
 
-  const ingredients = toast.map(ingredient => {
+  const ingredients = ingredientsArray.map(ingredient => {
     return { name: ingredient }
   })
 
