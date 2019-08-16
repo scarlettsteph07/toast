f4113505b7409f487897e02a375708ac715c0c34
Scarlett Tejada <scarlettsteph07@gmail.com>
2019-08-15 18:59:10 -0700

    Update requiredIngredients structure and helperMethods

--- helperMethods.ts
+++ helperMethods.ts
@@ -1,4 +1,4 @@
-import { requiredIngredients, requiredIngredient2 } from './config.json'
+import { requiredIngredients } from './config.json'
 
 import { Ingredient, Preferences } from './types/'
 
@@ -6,26 +6,31 @@ export const getRandomArrayIndex = (array: Array<string> | Array<Ingredient>): n
   return Math.floor(Math.random() * array.length)
 }
 
-export const getIngredientsHelper = ({ isCarnivore, numOfOptionalIngredients }: Preferences) => {
-  let { optionalIngredients } = require("./config.json")
-  const ingredientsArray = [...requiredIngredients]
-  const ingredient2Index = getRandomArrayIndex(requiredIngredient2)
-  ingredientsArray.push(requiredIngredient2[ingredient2Index])
+export const getIngredientStyle = (styleArray: Array<string>) => {
+  const index = getRandomArrayIndex(styleArray)
+  return styleArray[index]
+}
 
+export const getIngredientsHelper = ({ isCarnivore, numOfOptionalIngredients }: Preferences) => {
+  const initialIngredients = requiredIngredients.map(requiredIngredient => {
+    return getIngredientStyle(requiredIngredient.style)
+  })
+  const ingredientsArray = [...initialIngredients]
+
+  let { optionalIngredients } = require("./config.json")
   if (isCarnivore) {
     const meat = optionalIngredients.find(
       (ingredient: Ingredient) => ingredient.name === "meat"
     )
-    console.log(meat)
-    meat !== undefined && meat !== null && ingredientsArray.push(meat.style[getRandomArrayIndex(meat.style)])
+    meat !== undefined && meat !== null && ingredientsArray.push(getIngredientStyle(meat.style))
     numOfOptionalIngredients--
   }
 
   for (let x = 0; x < numOfOptionalIngredients; x++) {
     const optionalIngredientIndex = getRandomArrayIndex(optionalIngredients)
     const optionalIngredient = optionalIngredients[optionalIngredientIndex]
-    const optionalStyleIndex = getRandomArrayIndex(optionalIngredient.style)
-    ingredientsArray.push(optionalIngredient.style[optionalStyleIndex])
+    ingredientsArray.push(getIngredientStyle(optionalIngredient.style))
+
     optionalIngredients = optionalIngredients.filter((ingredient: Ingredient) => {
       return ingredient !== optionalIngredients[optionalIngredientIndex]
     })
