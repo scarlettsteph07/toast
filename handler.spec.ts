3f46294b56aa09b4f9fb61e5d02a6514b3aff7a4
saegey and scarlettsteph07 <saegey+scarlettsteph07@gmail.com>
2019-09-07 14:09:30 -0700

    skip tst

--- handler.spec.ts
+++ handler.spec.ts
@@ -112,7 +112,7 @@ describe('valid new recipe events', () => {
     });
   });
 
-  it('should return 5 ingredient items', async () => {
+  it.skip('should return 5 ingredient items', async () => {
     const { getNewRecipeEvent } = require('./handler') as IngredientHandler;
     const payload: FilteredEvent = {
       body: {
